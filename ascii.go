fb856177c934974fbb6ca50a1b0f4b50926cc620
BubbleFish <daforsastudia@gmail.com>
2025-10-02 23:21:10 +0300

    feat: Add ASCII control codes

---
+++ ascii.go
@@ -0,0 +1,40 @@
+package escapes
+
+// https://www.ascii-code.com/
+const (
+	// ASCII control characters (character code 0-31)
+	AsciiNull = iota
+	AsciiStartOfHeading
+	AsciiStartOfText
+	AsciiEndOfText
+	AsciiEndOfTransmission
+	AsciiEnquiry
+	AsciiAcknowledge
+	AsciiBell
+	AsciiBackspace
+	AsciiHorizontalTab
+	AsciiLineFeed
+	AsciiVerticalTab
+	AsciiFormFeed
+	AsciiCarriageReturn
+	AsciiShiftOut
+	AsciiShiftIn
+	AsciiDataLinkEscape
+	AsciiDeviceControlOne
+	AsciiDeviceControlTwo
+	AsciiDeviceControlThree
+	AsciiDeviceControlFour
+	AsciiNegativeAcknowledge
+	AsciiSyncIdle
+	AsciiEndOfTransmissionBlock
+	AsciiCancel
+	AsciiEndOfMedium
+	AsciiSubstitute
+	AsciiEscape
+	AsciiFileSeparator
+	AsciiGroupSeparator
+	AsciiRecordSeparator
+	AsciiUnitSeparaTOR
+
+	AsciiDelete = 127
+)
